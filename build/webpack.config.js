bf2a3a99e9415dadde5e5b2e008c7e6129fe4154
Sahil Maniar <sahil.mai@media.net>
2022-11-27 15:58:50 +0530

    Changes for webpack output directory

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path')
 
 module.exports = {
-    entry: './src/background,js',
+    entry: './src/background.js',
     output: {
         filename: 'background.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve('dist')
     }
 }
